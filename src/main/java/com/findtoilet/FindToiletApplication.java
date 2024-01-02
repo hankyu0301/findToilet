7e5bb5893cedc3ab45f011d72cf530026fa7ef45
hankyujang <finebears@naver.com>
2024-01-02 19:28:43 +0900

    git init

---
+++ src/main/java/com/findtoilet/FindToiletApplication.java
@@ -0,0 +1,13 @@
+package com.findtoilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindToiletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindToiletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/findtoilet/FindToiletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findtoilet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindToiletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
