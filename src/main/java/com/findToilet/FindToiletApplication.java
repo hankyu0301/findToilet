5bdea9125a14f9e749fe0d5332fb3937ccc79058
hankyujang <finebears@naver.com>
2024-01-15 16:12:55 +0900

    refactor : 패키지 명 수정

--- src/main/java/com/findToilet/FindToiletApplication.java
+++ src/main/java/com/findToilet/FindToiletApplication.java
@@ -1,4 +1,4 @@
-package com.findtoilet;
+package com.findToilet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/findToilet/FindToiletApplicationTests.java
+++ src/test/java/com/findToilet/FindToiletApplicationTests.java
@@ -1,4 +1,4 @@
-package com.findtoilet;
+package com.findToilet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
