065d8e78dfa8aeb28ba05df2b3cd92fd2e78325d
hankyujang <finebears@naver.com>
2024-01-30 21:04:39 +0900

    refactor : MemberRepository에 findByEmail()메서드 추가

--- src/main/java/com/findToilet/domain/member/repository/MemberRepository.java
+++ src/main/java/com/findToilet/domain/member/repository/MemberRepository.java
@@ -3,6 +3,11 @@ package com.findToilet.domain.member.repository;
 import com.findToilet.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
+
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
