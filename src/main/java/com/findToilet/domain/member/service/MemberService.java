a18c5815cafbac22e220cd809e06364674e06ce1
hankyujang <finebears@naver.com>
2024-02-06 17:05:18 +0900

    refactor : 오타 수정

--- src/main/java/com/findToilet/domain/member/service/MemberService.java
+++ src/main/java/com/findToilet/domain/member/service/MemberService.java
@@ -29,7 +29,7 @@ public class MemberService {
         return Member.builder()
                 .email(req.getEmail())
                 .password(passwordEncoder.encode(req.getPassword()))
-                .nickname(req.getPassword())
+                .nickname(req.getNickname())
                 .build();
     }
 
